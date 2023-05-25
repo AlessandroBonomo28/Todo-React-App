3d19d02248f75756931b2b60824ef2d51f769791
Alexbonny28 <al.bonomo@studenti.unina.it>
2023-05-25 20:23:34 +0200

    setup

--- todo-app/src/components/CustomSpan.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { COLORS } from '../constants';
-export const CustomSpan = styled.span`
-  font-size: 25px;
-  font-weight: bold;
-  color: ${(props) =>
-    props.color? props.color : COLORS.primaryTextColor};
-`;
